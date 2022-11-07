1551b04f4c75b77b4337098624430f20f5d4c2cc
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-11-07 11:12:35 +0900

    finish seeds

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,3 +1,7 @@
 export const toUpperSnakeCase = (str: string) => {
   return str.replace(/[^a-zA-Z]/, '_').toUpperCase();
 };
+
+export const randomIndex = (arr: any[]) => {
+  return Math.floor(Math.random() * arr.length);
+};
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,18 @@
 import { faker } from '@faker-js/faker';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, ProductCategory, ProductStatus } from '@prisma/client';
+import { randomIndex } from '../lib/helpers';
+import { toUpperSnakeCase } from './../lib/helpers';
+import data from './data';
 
 const prisma = new PrismaClient();
 
 console.log('Seeding database...');
 
 async function main() {
+  await prisma.user.deleteMany();
+
+  const users = [];
+
   for (let i = 0; i < 10; i++) {
     const name = faker.name.fullName();
     const username = faker.internet.userName();
@@ -21,8 +28,37 @@ async function main() {
       },
     });
 
+    users.push(user);
+
     console.log(`Created user: ${user.username}`);
   }
+
+  data.productRequests.forEach(
+    async ({ id, comments, status, category, ...product }) => {
+      const createdProduct = await prisma.product.create({
+        data: {
+          ...product,
+          status: toUpperSnakeCase(status) as ProductStatus,
+          category: toUpperSnakeCase(category) as ProductCategory,
+          userId: users[randomIndex(users)].id,
+        },
+      });
+
+      console.log(`Created product: ${createdProduct.title}`);
+
+      comments?.forEach(async (comment) => {
+        const createdComment = await prisma.comment.create({
+          data: {
+            content: comment.content,
+            userId: users[randomIndex(users)].id,
+            productId: createdProduct.id,
+          },
+        });
+
+        console.log(`Created comment: ${createdComment.content}`);
+      });
+    },
+  );
 }
 
 main();
